1a08396840758b98c0ddf0dcd2f5dba467e6d3f6
qtcyy <qtcyy2005@gmail.com>
2025-03-07 21:27:50 +0800

    style: 💄 美化Loading页面

--- apps/web/src/page/BasePage/LoadingPage.tsx
+++ apps/web/src/page/BasePage/LoadingPage.tsx
@@ -1,5 +1,23 @@
+import { IconLoading } from "@arco-design/web-react/icon";
+import styled from "styled-components";
+
 const LoadingPage = () => {
-  return <div className="text-3xl">Loading...</div>;
+  return (
+    <LayoutWrapper className="h-screen w-screen">
+      <div className="text-6xl">
+        <IconLoading />
+      </div>
+    </LayoutWrapper>
+  );
 };
+
+const LayoutWrapper = styled.div`
+  background: ${({ theme }) => theme.colors.background};
+  color: ${({ theme }) => theme.colors.text};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export { LoadingPage };
