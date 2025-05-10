ef2b7c3afd80a0d8ff45ac809984dab70a218f72
cheng yiyang <qtcyy2005@gmail.com>
2025-05-10 14:58:27 +0800

    feat: 🎸 暂存页面

--- apps/web/src/page/chat/MoreApps/MoreAppsPage.tsx
+++ apps/web/src/page/chat/MoreApps/MoreAppsPage.tsx
@@ -1,6 +1,10 @@
 import { SelectedLine } from "components";
-import { JSX, useState } from "react";
+import { JSX, useEffect, useRef, useState } from "react";
+import SimpleBar from "simplebar-react";
+import SimpleBarCore from "simplebar-core";
 import styled from "styled-components";
+import { Avatar, Button } from "@arco-design/web-react";
+import { useTheme } from "theme";
 
 type ItemType = {
   id: string;
@@ -8,7 +12,17 @@ type ItemType = {
   href?: string;
 };
 
-const appItems: ItemType[] = [
+export type AppsType = {
+  id: string;
+  top?: boolean;
+  classify: string;
+  name: string;
+  description?: string;
+  from?: string;
+  icon?: string;
+};
+
+const classItems: ItemType[] = [
   {
     id: "1",
     name: "我的应用",
@@ -19,39 +33,151 @@ const appItems: ItemType[] = [
   },
 ];
 
+const appItems: AppsType[] = [
+  {
+    id: "1",
+    classify: "2",
+    top: true,
+    name: "写作助手",
+    description: "完成你的写作任务",
+    from: "cyy",
+  },
+  {
+    id: "2",
+    classify: "2",
+    name: "翻译助手",
+    description: "你的专属翻译助手",
+    from: "GitHub",
+  },
+];
+
 const MoreAppsPage = (): JSX.Element => {
   const [itemSelected, setItemSelected] = useState<string>("1");
+  const [scrollTop, setScrollTop] = useState<number | undefined>(undefined);
+  const { theme } = useTheme();
+
+  const pageRef = useRef<SimpleBarCore>(null);
+  const target = pageRef.current?.getScrollElement();
+
+  const getScrollInfo = () => {
+    setScrollTop(target?.scrollTop);
+    console.log(scrollTop);
+  };
+
+  useEffect(() => {
+    console.log(scrollTop);
+  }, [scrollTop]);
 
   return (
-    <ContentWrapper>
-      <div className="w-full flex items-center justify-center py-4 text-2xl font-bold">
-        更多应用
-      </div>
-      <div className="flex text-xl font-bold">探索</div>
-      <ListWrapper>
-        {appItems.map((item) => {
-          const handleClick = () => {
-            setItemSelected(item.id);
-          };
-          const isSelected = itemSelected == item.id;
+    <SimpleBar
+      ref={pageRef}
+      onScrollCapture={getScrollInfo}
+      style={{ overflow: "auto", width: "100%", height: "100vh" }}
+    >
+      <ContentWrapper>
+        <div className="w-full flex items-center justify-center py-4 text-2xl font-bold">
+          更多应用
+        </div>
+        <div className="flex text-xl font-bold">探索</div>
+        <ListWrapper>
+          {classItems.map((item) => {
+            const handleClick = () => {
+              setItemSelected(item.id);
+            };
+            const isSelected = itemSelected == item.id;
 
-          return (
-            <ItemWrapper
-              key={item.id}
-              $selected={isSelected}
-              onClick={handleClick}
-            >
-              {item.name}
-              {isSelected && (
-                <SelectedLine top={"26px"} width={"24px"} height={"2px"} />
-              )}
-            </ItemWrapper>
-          );
-        })}
-      </ListWrapper>
-    </ContentWrapper>
+            return (
+              <ItemWrapper
+                key={item.id}
+                $selected={isSelected}
+                onClick={handleClick}
+              >
+                {item.name}
+                {isSelected && (
+                  <SelectedLine top={"26px"} width={"24px"} height={"2px"} />
+                )}
+              </ItemWrapper>
+            );
+          })}
+          <Button type="primary" onClick={getScrollInfo}>
+            Get
+          </Button>
+        </ListWrapper>
+        <EnumWrapper>
+          {classItems.map((classItem) => {
+            return (
+              <>
+                <div key={classItem.id} className="mt-4 mb-2 text-md font-bold">
+                  {classItem.name}
+                </div>
+                <AppListWrapper>
+                  {appItems.map((appItem) => {
+                    if (
+                      appItem.classify == classItem.id ||
+                      (appItem.top && classItem.id == "1")
+                    ) {
+                      return (
+                        <AppItemWrapper>
+                          <Avatar size={60} shape="circle">
+                            <img src={appItem.icon} />
+                          </Avatar>
+                          <div className="flex flex-col">
+                            <div className="font-bold text-lg">
+                              {appItem.name}
+                            </div>
+                            <div className={`text-[${theme.colors.secondary}]`}>
+                              {appItem.description}
+                            </div>
+                            <div className="mt-2">
+                              {"来自: " + appItem.from}
+                            </div>
+                          </div>
+                        </AppItemWrapper>
+                      );
+                    } else {
+                      return null;
+                    }
+                  })}
+                </AppListWrapper>
+              </>
+            );
+          })}
+        </EnumWrapper>
+      </ContentWrapper>
+    </SimpleBar>
   );
 };
+
+const TestBlock = styled.div`
+  height: 1500px;
+`;
+
+const AppListWrapper = styled.ul`
+  display: flex;
+  box-sizing: border-box;
+  width: 100%;
+  flex-wrap: wrap;
+  list-style: none;
+  justify-content: space-between;
+`;
+
+const AppItemWrapper = styled.li`
+  width: calc((100% - 16px) / 2);
+  margin-bottom: 16px;
+  padding: 12px;
+  border-radius: 12px;
+  background: ${({ theme }) => theme.colors.componentBg};
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  height: 104px;
+  cursor: pointer;
+`;
+
+const EnumWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const ItemWrapper = styled.li<{ $selected?: boolean }>`
   display: flex;
@@ -74,7 +200,9 @@ const ListWrapper = styled.ul`
   display: flex;
   position: sticky;
   flex-direction: row;
+  top: 20px;
   gap: 30px;
+  z-index: 10;
   list-style: none;
   margin-top: 20px;
   margin-bottom: 20px;
