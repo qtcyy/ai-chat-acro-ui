4b7d57ca358dbd9e780def1443b5c3bbe0de1561
cheng yiyang <qtcyy2005@gmail.com>
2025-08-12 10:22:14 +0800

    feat: 🎸 新建web2结构

--- apps/web2/src/App.tsx
+++ apps/web2/src/App.tsx
@@ -1,8 +1,20 @@
+import { AxiosProvider } from "utils";
+import NiceModal from "@ebay/nice-modal-react";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import { routes } from "./routes/routes";
+import { ThemeProvider } from "theme";
+
+const router = createHashRouter(routes);
+
 const App = () => {
   return (
-    <div className=" h-full m-4">
-      <div className="text-2xl">Hello, World!</div>
-    </div>
+    <ThemeProvider>
+      <AxiosProvider baseUrl="http://0.0.0.0:8000/">
+        <NiceModal.Provider>
+          <RouterProvider router={router} />
+        </NiceModal.Provider>
+      </AxiosProvider>
+    </ThemeProvider>
   );
 };
 
---
+++ apps/web2/src/page/home/TestHome.tsx
@@ -0,0 +1,9 @@
+const TestHome = () => {
+  return (
+    <div className="h-full p-4">
+      <div className="text-2xl">Hello, World!</div>
+    </div>
+  );
+};
+
+export { TestHome };
