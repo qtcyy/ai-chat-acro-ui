8f755100862616a79a157d8cc48bb888f3c45071
cheng yiyang <qtcyy2005@gmail.com>
2025-08-12 08:34:38 +0800

    feat: 🎸 init web2

---
+++ apps/web2/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className=" h-full">
+      <div className="text-2xl">Hello, World!</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ apps/web2/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,tsx}",
+    "../../packages/components/src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
